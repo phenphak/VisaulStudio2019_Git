d3f1cc1a39913cef288649313a2f9741e60e0e3c
phenphak manphoom <phenphaktae@hotmail.com>
2020-06-29 17:23:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisaulStudio2019_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
