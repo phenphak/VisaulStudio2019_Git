25b48ae4be8e57f8a96afaee960ebde74f35376d
phenphak manphoom <phenphaktae@hotmail.com>
2020-07-01 17:20:13 +0700

    Commit the change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VisaulStudio2019_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Here is a change");
         }
     }
 }
